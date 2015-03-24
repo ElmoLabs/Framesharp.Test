effc217d56cefebe2246c226e7e062774e463b19
claudiocidade <claudiocidade@hotmail.com>
2015-03-24 00:50:22 -0300

    General refactoring

---
+++ Framesharp.Test/TestClassBase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Framesharp.Configuration;
+using Framesharp.Core.Interfaces;
+using Framesharp.DependencyInjection;
+using Framesharp.Test.Data;
+
+namespace Framesharp.Test
+{
+    public abstract class TestClassBase : IOperationCaller, IDisposable
+    {
+        public IOperationCallContext OperationCallContext { get; private set; }
+
+        protected ApplicationSettings ApplicationSettings { get; private set; }
+
+        protected EquivalencePartitioning EquivalencePartition { get; private set; }
+
+        protected TestClassBase(ApplicationSettings applicationSettings)
+        {
+            ApplicationSettings = applicationSettings;
+
+            DependencyResolver.ConfigureApplication(applicationSettings);
+
+            EquivalencePartition = DependencyResolver.GetInstance<EquivalencePartitioning>();
+        }
+
+        public void Dispose()
+        {
+            OperationCallContext.Dispose();
+        }
+    }
+}
